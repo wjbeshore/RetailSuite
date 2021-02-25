a3c0aecbb22ec37ab686be01d73a735f42dd0ddf
will <wjbeshore@gmail.com>
2021-02-24 18:19:47 -0600

    Working on node input

--- app/input_test.js
+++ app/input_test.js
@@ -2,8 +2,16 @@ const parse = require('csv-parse');
 const fs = require('fs');
 var stringify = require('csv-stringify');
 
+
 var parser = parse(function (err, records) {
-	console.log(records[40]);
+    let temp_arr = [];
+	// console.log(records[40]);
+    records.forEach(element => (temp_arr.push(element.filter(item => item != ""))));
+    console.log(temp_arr[45]);
+    let stores = {};
+    temp_arr = temp_arr.slice(11);
+    temp_arr = temp_arr.slice(0,-3);
+
 });
 
 fs.createReadStream('Apr19.csv').pipe(parser);
