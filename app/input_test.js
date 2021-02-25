f471f733f1a92000939c92d0785dcd21f9656bd4
will <wjbeshore@gmail.com>
2021-02-25 15:13:59 -0600

    More work converting python upload to js

--- app/input_test.js
+++ app/input_test.js
@@ -7,11 +7,40 @@ var parser = parse(function (err, records) {
     let temp_arr = [];
 	// console.log(records[40]);
     records.forEach(element => (temp_arr.push(element.filter(item => item != ""))));
-    console.log(temp_arr[45]);
     let stores = {};
     temp_arr = temp_arr.slice(11);
     temp_arr = temp_arr.slice(0,-3);
+    let date = temp_arr[1][5];
+    let current_store = "";
+    temp_arr.forEach(item => {
+        // console.log(item);
+        if(item[0] == "Donor Total"){
+            current_store = "";
+        } else if((isNaN(item[1])) && (item[1] != "RETAIL") ){
+            current_store = parseInt(item[0])
+            stores[current_store] = [];
+        } else{
+            if(item[8] == '12/31/9999'){
+                
+                let pounds = item[9];
+                pounds = pounds.slice(0,-1);
+                stores[current_store].push([item[5], item[2], item[4], parseFloat(pounds)])
+                
+            } else {
+                
+                let pounds = item[8];
+                pounds = pounds.slice(0,-1);
+                stores[current_store].push([item[5], item[2], item[4], parseFloat(pounds)])
+                
+            }
+                
 
+            }
+        
+    })
+    
+        
+    console.log(stores);
 });
 
 fs.createReadStream('Apr19.csv').pipe(parser);
